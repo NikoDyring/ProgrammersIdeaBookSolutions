15d2722c0301e043ab23a845b99b6a01b335ff9c
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2018-03-13 02:21:42 +0100

    Simple Assignment
    Added difficulty will be done later. Simple tax calculator.

---
+++ Numbers/Difficulty 1/Tax Calculator/TaxCalculator/TaxCalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    class Calculator
+    {
+        double sum = 0;
+
+        public double CalculateTax()
+        {
+            Console.WriteLine("Please enter a value");
+            double value = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the tax percentage");
+            double tax = Convert.ToDouble(Console.ReadLine());
+            double convertedTax = tax / 100 + 1;
+            sum = (convertedTax) * value;
+
+            return sum;
+        }
+    }
+}
