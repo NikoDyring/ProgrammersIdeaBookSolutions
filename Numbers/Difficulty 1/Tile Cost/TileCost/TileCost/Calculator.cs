bb262bd1f48d8171e7a920893625cd51331299f5
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2018-03-13 02:04:53 +0100

    TileCost Simple added.
    Simple Assignment done, looking into Added difficulty at a later time.

---
+++ Numbers/Difficulty 1/Tile Cost/TileCost/TileCost/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileCost
+{
+    public class Calculator
+    {
+
+        private double totalCost = 0;
+        private double area = 0;
+
+        public double CalculateCost()
+        {
+            double length = Convert.ToDouble(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
+            double costPrSqm = Convert.ToDouble(Console.ReadLine());
+            area = length * width;
+            totalCost = costPrSqm * area;
+
+            return totalCost;
+        }
+    }
+}
---
+++ Numbers/Difficulty 1/Tile Cost/TileCost/TileCost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileCost
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Please provide width, length and cost pr sqm.");
+            Console.WriteLine("The total cost is " + calc.CalculateCost() + "$");
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
