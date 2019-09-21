70aa07616330d2761d9e2abe99d0acabbe998856
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2019-09-21 03:06:20 +0200

    WordCounter Added

---
+++ WordCounter/WordCounter/WordCounter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string testString =
+                "I'm just not into trying to convince people like me. I always say to myself," +
+                " It is what it is.' I walk into a situation knowing that people are either" +
+                " going to love me or they're not, and that's OK." +
+                " I'm just going to be me. You can't be everything to everyone.";
+
+            Console.WriteLine("The total amount of words in the given string is: " + WordCounter.CountWords(testString));
+            Console.WriteLine("------------------");
+            Console.WriteLine("Press any key to exit the program..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WordCounter/WordCounter/WordCounter/WordCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class WordCounter
+    {
+
+        public static int CountWords(string s)
+        {
+            // By using the Regex method "Matches" we can check our input for patterns. In this case, looking for whitespaces (/s).
+            MatchCollection collection = Regex.Matches(s, @"[\S]+");
+            return collection.Count;
+        }
+
+    }
+}
