0de69625e8c89f353f914db8c15f2a9485b144b8
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2019-09-22 00:17:01 +0200

    PalindromeChecker

---
+++ PalindromeChecker/PalindromeChecker/PalindromeChecker/PalindromeChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker
+{
+    class PalindromeChecker
+    {
+        private static string _secondPart;
+
+        public static string Check(string input)
+        {
+            char[] characterArray = input.ToCharArray();
+
+            Array.Reverse(characterArray);
+            _secondPart = new string(characterArray);
+
+            bool isPalindrome = input.Equals(_secondPart.ToLower());
+            if (isPalindrome)
+            {
+                return input + " is a palindrome!";
+            }
+            else
+            {
+                return input + " is not a palindrome!";
+            }
+        }
+
+
+    }
+}
---
+++ PalindromeChecker/PalindromeChecker/PalindromeChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE PALINDROME CHECKER!\n" +
+                              "Type in your word you want to have checked if it's a\n" +
+                              "palindrome or not!");
+            Console.WriteLine("--------------------------");
+            string input = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(PalindromeChecker.Check(input));
+            Console.WriteLine("Press any key to close the program..");
+            Console.ReadKey();
+
+
+        }
+    }
+}
